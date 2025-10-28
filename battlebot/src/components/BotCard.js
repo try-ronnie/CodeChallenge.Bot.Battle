2313a24370ee1e7248b62b024e645ebc7372747c
ronniegikandi <kiiruronnie42@gmil.com>
2025-10-28 12:18:58 +0300

    style part commit

--- battlebot/src/components/BotCard.js
+++ battlebot/src/components/BotCard.js
@@ -3,3 +3,11 @@ return (
 <div className="bot-card" onClick={onClick}>
 <img src={bot.avatar_url} alt={bot.name} width="150" />
 <h3>{bot.name}</h3>
+<p>{bot.bot_class}</p>
+<p>{bot.health} {bot.damage} {bot.armor}</p>
+{onDelete && (
+<button
+onClick={(e) => {
+e.stopPropagation();
+onDelete(bot);
+}}
