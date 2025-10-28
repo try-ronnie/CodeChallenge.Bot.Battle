d3aed60a2ef3f9b6a84baf15bd612359b35d59f3
ronniegikandi <kiiruronnie42@gmil.com>
2025-10-28 12:16:30 +0300

    starting botcard logic

--- battlebot/src/components/BotCard.js
+++ battlebot/src/components/BotCard.js
@@ -1,23 +1,5 @@
 function BotCard({ bot, onClick, onDelete }) {
-  return (
-    <div className="bot-card" onClick={onClick}>
-      <img src={bot.avatar_url} alt={bot.name} width="150" />
-      <h3>{bot.name}</h3>
-      <p>{bot.bot_class}</p>
-      <p>{bot.health} {bot.damage} {bot.armor}</p>
-      {onDelete && (
-        <button
-          onClick={(e) => {
-            e.stopPropagation();
-            onDelete(bot);
-          }}
-          style={{ color: "red" }}
-        >
-          X
-        </button>
-      )}
-    </div>
-  );
-}
-
-export default BotCard;
+return (
+<div className="bot-card" onClick={onClick}>
+<img src={bot.avatar_url} alt={bot.name} width="150" />
+<h3>{bot.name}</h3>
